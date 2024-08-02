15a841a8c7009f251a6dfb6c5a422d742de06516
sharwarigole <sharwarigole@gmail.com>
2024-08-02 16:16:57 +0530

    add project description in README.md

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # RockPaperScissors
-Rock Paper Scissor game in Javascript
+This is a simple console-based Rock Paper Scissors game implemented in JavaScript. The project is part of the assignments from The Odin Project.
+
+# Table Of Contents
+
+* Overview
+* Features 
+* How To Play
+* Installation
+* Usage
+* Contributing
+
+# Overview
+
+This project is a basic implementation of the classic Rock Paper Scissors game. The game is played in the console, where the player competes against the computer. The player and the computer choose one of Rock, Paper, or Scissors, and the winner is determined based on the rules of the game.
+
+# Features
+
+* Console-based gameplay
+* Random computer moves
+* Player input validation
+* Score tracking
+
+# How to Play
+
+1. The game will prompt you to enter your choice(Rock, Paper, or Scissors).
+2. The computer will randomly choose its move.
+3. The winner is determined and displayed in the console.
+4. The game keeps track of the score for both the player and the computer.
+
+# Installation
+
+To run this game locally, you'll need to have Node.js installed on your machine.
+
+1. Clone the repository:
+
+```
+git clone https://github.com/your-username/rock-paper-scissors.git
+
+```
+2. Navigate to the project directory:
+
+```
+cd rock-paper-scissors
+
+```
+
+# Usage
+
+To start the game, open your terminal and run the following command:
+
+```
+node game.js
+```
+
+Follow the on-screen prompts to play the game.
+
+# Contributing
+
+Contributions are welcome! If you have suggestions for improvements or new features, please feel free to submit a pull request or open an issue.
+
+
--- game.js
+++ game.js
@@ -8,7 +8,83 @@ function getComputerChoice()
     let randomNumber = Math.floor(Math.random()*30) + 1;
     console.log(randomNumber);
 
+    if(1 <= randomNumber && randomNumber <= 10)
+    {
+        return rock;
+    }
+    else if(11 <= randomNumber && randomNumber <= 20)
+    {
+        return paper;
+    }
+    else
+    {
+        return scissors;
+    }
     
 }
 
-getComputerChoice();
+function getHumanChoice()
+{
+    let humanChoice = prompt("Enter your choice : Rock or Paper or Scissors?");
+    return humanChoice;
+}
+
+const humanSelection = getHumanChoice();
+console.log(humanSelection);
+
+const computerSelection = getComputerChoice()
+console.log(computerSelection);
+
+// playRound(humanSelection,computerSelection);
+
+function playGame()
+{
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice)
+    {
+        humanChoice = humanChoice.toLowerCase();
+        console.log(humanChoice);
+
+        if(humanChoice === computerChoice)
+        {
+            console.log("Oops! It's a tie!");
+        }
+
+        if(humanChoice === "rock" && computerChoice === "paper")
+        {
+            console.log("You loose! Paper beats rocks");
+            computerScore++;
+        }
+        else if(humanChoice === "paper" && computerChoice === "scissor")
+        {
+            console.log("You loose! Scissor beats paper");
+            computerScore++;
+        }
+        else if(humanChoice === "scissor" && computerChoice === "rock")
+        {
+            console.log("You loose! Rock beats scissor");
+            computerScore++;
+        }
+        else
+        {
+            console.log("You win!");
+            humanScore++;
+        }
+
+    }
+
+    let i = 1;
+    while(i <= 5)
+    {
+        playRound(humanSelection,computerSelection);
+    }
+
+    if(humanScore > computerScore)
+    {
+        console.log("Congratulations you won this round!");
+    }
+}
+
+playGame();
