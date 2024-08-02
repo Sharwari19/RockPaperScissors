a4f855528e97621efc4aaf606015dbe9d40ab853
sharwarigole <sharwarigole@gmail.com>
2024-08-02 14:58:47 +0530

    write logic to generate a random number between 1 and 30

---
+++ game.js
@@ -0,0 +1,14 @@
+// Rock Paper Scissors
+
+function getComputerChoice()
+{
+    let rock = "rock";
+    let paper = "paper";
+    let scissors = "scissors";
+    let randomNumber = Math.floor(Math.random()*30) + 1;
+    console.log(randomNumber);
+
+    
+}
+
+getComputerChoice();
