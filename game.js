e923b7ac57095536f5512cc4cafdd776fad1e682
sharwarigole <sharwarigole@gmail.com>
2024-08-18 10:19:36 +0530

    show if the user won or lost based on comparison

--- game.js
+++ game.js
@@ -35,5 +35,15 @@ function playRound()
 
     roundCount++;
 
-    
+    if( humanChoice === RPS[0] && computerChoice === RPS[1] ||
+        humanChoice === RPS[1] && computerChoice === RPS[2] ||
+        humanChoice === RPS[2] && computerChoice === RPS[0] 
+    )
+    {
+        console.log(`You loose! ${computerChoice} beats ${humanChoice}`);
+    }
+    else 
+    {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    }
 }
