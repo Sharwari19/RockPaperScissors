8f57c3093139255d041cf8d96edceddffc2b95b1
sharwarigole <sharwarigole@gmail.com>
2024-08-18 10:06:02 +0530

    generate a function to get computer choice

--- game.js
+++ game.js
@@ -2,3 +2,9 @@
 
 let RPS = ["Rock", "Paper", "Scissors"];
 
+function getComputerChoice()
+{
+    let randomNumber = Math.floor(Math.random()*3);
+    console.log('randomNumber : ', randomNumber);
+    return RPS[randomNumber];
+}
