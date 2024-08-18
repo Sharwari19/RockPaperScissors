c1659cf45ab7b06b0e562dbb48ee22a9e1d549b3
sharwarigole <sharwarigole@gmail.com>
2024-08-18 10:56:16 +0530

    restart the game after every 5 rounds

--- game.js
+++ game.js
@@ -24,6 +24,25 @@ function getHumanChoice()
 let humanScore = 0;
 let computerScore = 0;
 let roundCount = 0;
+const humanSelection = document.getElementById('humanChoice');
+const computerSelection = document.getElementById('computerChoice');
+const count = document.getElementById('roundNo');
+const humanRecord = document.getElementById('humanScore');
+const computerRecord = document.getElementById('computerScore');
+
+function RestartGame()
+{
+    humanScore = 0;
+    computerScore = 0;
+    roundCount = 0;
+
+   humanSelection.textContent = `Human Choice : `;
+   computerSelection.textContent = `Computer Choice : `;
+   count.textContent = `Round : ${roundCount}`;
+   humanRecord.textContent = `Human Score : ${humanScore}`;
+   computerRecord.textContent = `Computer Score : ${computerScore}`;
+   document.getElementById('restart').textContent = "Game Restarted!";
+}
 
 function playRound(event)
 {
@@ -35,11 +54,11 @@ function playRound(event)
 
     roundCount++;
 
-    document.getElementById('humanChoice').textContent = `Human Choice : ${humanChoice}`;
-    document.getElementById('computerChoice').textContent = `Computer Choice : ${computerChoice}`;
-    document.getElementById('roundNo').textContent = `Round : ${roundCount}`;
-    document.getElementById('humanScore').textContent = `Human Score : ${humanScore}`;
-    document.getElementById('computerScore').textContent = `Computer Score : ${computerScore}`;
+    humanSelection.textContent = `Human Choice : ${humanChoice}`;
+    computerSelection.textContent = `Computer Choice : ${computerChoice}`;
+    count.textContent = `Round : ${roundCount}`;
+    humanRecord.textContent = `Human Score : ${humanScore}`;
+    computerRecord.textContent = `Computer Score : ${computerScore}`;
 
 
     if( humanChoice === RPS[0] && computerChoice === RPS[1] ||
@@ -57,22 +76,29 @@ function playRound(event)
     if((humanScore > computerScore) && roundCount === 6)
     {
         document.getElementById('winner').textContent = `Round Winner : You!`;
+        RestartGame();
     }
     else if((humanScore < computerScore) && roundCount === 6)
     {
         document.getElementById('winner').textContent = `Round Winner : Computer!`;
+        RestartGame();
     }
 }
 
 const Rock = document.createElement("button");
 const Paper = document.createElement("button");
 const Scissors = document.createElement("button");
+const Restart = document.createElement("button");
 
 Rock.textContent = "Rock";
 Paper.textContent = "Paper";
 scissor.textContent = "Scissors";
+Restart.textContent = "Restart";
 
 const div = document.querySelector('content');
+const restartDiv = document.getElementById('restart');
+restartDiv.appendChild(Restart);
+Restart.addEventListener('click', RestartGame);
 
 [Rock, Paper, Scissors].forEach(button => {
     div.appendChild(button);
